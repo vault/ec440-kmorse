4249d75921bf93c2d3e42da570c5e4742a4c1ca7
Michael Abed <michaelabed@gmail.com>
2013-04-07 15:19:24 -0400

    hello world module

---
+++ message_mod.c
@@ -0,0 +1,15 @@
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void)
+{
+    printk(KERN_INFO, "Hello world 1.\n");
+
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    printk(KERN_INFO, "Goodbye wold 1.\n");
+}
