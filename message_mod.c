c840d38c13c28687d54d7a1a254151c664118c6c
Michael Abed <michaelabed@gmail.com>
2013-04-07 15:25:12 -0400

    Fix args

--- message_mod.c
+++ message_mod.c
@@ -4,12 +4,12 @@
 
 int init_module(void)
 {
-    printk(KERN_INFO, "Hello world 1.\n");
+    printk(KERN_INFO "Hello world 1.\n");
 
     return 0;
 }
 
 void cleanup_module(void)
 {
-    printk(KERN_INFO, "Goodbye wold 1.\n");
+    printk(KERN_INFO "Goodbye wold 1.\n");
 }
