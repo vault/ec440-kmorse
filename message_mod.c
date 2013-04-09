b6ac89960dcaaa4377f4988238778beb70e8fae9
Michael Abed <michaelabed@gmail.com>
2013-04-08 22:23:07 -0400

    Fix bug during intial access

--- message_mod.c
+++ message_mod.c
@@ -27,7 +27,7 @@ static void __exit message_exit(void)
 
     list_for_each_safe(pos, pos2, &message_list->list) {
         tmp = list_entry(pos, struct message, list);
-        __list_del_entry(pos);
+        list_del(pos);
         kfree(tmp);
     }
     printk(KERN_INFO "Unloaded message module\n");
@@ -36,6 +36,12 @@ static void __exit message_exit(void)
 static struct message* new_message(int minor)
 {
     struct message *msg = kmalloc(sizeof(struct message), GFP_KERNEL);
+    int i;
+
+    printk(KERN_INFO"message: creating message for minor %d\n", minor);
+
+    for (i = 0; i < 32; i++)
+        msg->buffer[i] = 0;
     msg->minor = minor;
     msg->open = 0;
     msg->pos = 0;
@@ -48,8 +54,14 @@ static struct message *find_message(int minor)
 {
     struct list_head *pos;
     struct message *tmp;
+
+    printk(KERN_INFO"message: searching for minor %d\n", minor);
+
+    if (message_list->minor == minor)
+        return message_list;
     list_for_each(pos, &message_list->list) {
         tmp = list_entry(pos, struct message, list);
+        printk(KERN_INFO"message: minor %d\n", tmp->minor);
         if (tmp->minor == minor)
             return tmp;
     }
@@ -58,6 +70,7 @@ static struct message *find_message(int minor)
 
 static void add_message(struct message *msg)
 {
+    printk(KERN_INFO"message: adding message for minor %d\n", msg->minor);
     list_add(&msg->list, &message_list->list);
 }
 
@@ -69,15 +82,20 @@ static int message_open(struct inode *inode, struct file *filp)
     min_data min;
     min.minor = minor;
 
+    printk(KERN_INFO"message: opened minor %d\n", minor);
+
     // Find or allocate the struct message for this minor
     if (message_list == NULL) {
+        printk(KERN_INFO"message: new message_list\n");
         message_list = new_message(minor);
         msg = message_list;
         INIT_LIST_HEAD(&message_list->list);
     } else {
+        printk(KERN_INFO"message: looking for message\n");
         msg = find_message(minor);
     }
     if (msg == NULL) {
+        printk(KERN_INFO"message: not found, allocing new\n");
         msg = new_message(minor);
         add_message(msg);
     }
@@ -88,6 +106,7 @@ static int message_open(struct inode *inode, struct file *filp)
     msg->open++;
     msg->pos = 0;
     filp->private_data = min.pt;
+    printk(KERN_INFO"message: finished open\n");
     return 0;
 }
 
@@ -95,6 +114,7 @@ static int message_release(struct inode *inode, struct file *filp)
 {
     struct message *msg = find_message(((min_data)filp->private_data).minor);
     if (msg != NULL) {
+        printk(KERN_INFO"message: closing message %d\n", msg->minor);
         msg->pos = 0;
         msg->open--;
     }
@@ -103,8 +123,10 @@ static int message_release(struct inode *inode, struct file *filp)
 
 static ssize_t message_read(struct file *filp, char *buffer, size_t len, loff_t *off)
 {
+    int to_copy, left;
     struct message *msg = find_message(((min_data)filp->private_data).minor);
-    int to_copy, copied;
+    if (msg == NULL)
+        return -EINVAL;
 
     if (!msg->valid)
         return -EINVAL;
@@ -112,9 +134,11 @@ static ssize_t message_read(struct file *filp, char *buffer, size_t len, loff_t 
     if (msg->pos >= msg->len)
         return 0;
 
-    to_copy = len>msg->len ? msg->len : len;
-    copied = copy_to_user(buffer, msg->buffer+msg->pos, to_copy);
-    if (copied) // we didn't finish copying
+    printk(KERN_INFO"message: reading from %d message \"%s\"\n", msg->minor, msg->buffer);
+
+    to_copy = len > msg->len ? msg->len : len;
+    left = copy_to_user(buffer, msg->buffer+msg->pos, to_copy);
+    if (left) // we didn't finish copying
         return -EINVAL;
 
     msg->pos += to_copy;
@@ -123,18 +147,24 @@ static ssize_t message_read(struct file *filp, char *buffer, size_t len, loff_t 
 
 static ssize_t message_write(struct file *filp, const char *buff, size_t len, loff_t *off)
 {
+    int to_copy;
     struct message *msg = find_message(((min_data)filp->private_data).minor);
-    int copied;
+    if (msg == NULL) {
+        return -EINVAL;
+    }
+
     if (msg->valid) {
         msg->valid = 0;
     }
 
-    copied = copy_from_user(msg->buffer, buff, 31);
-    if (copied) {
-        return -EINVAL;
-    }
+    to_copy = len > 31 ? 31 : len;
+    copy_from_user(msg->buffer, buff, to_copy);
+
+    printk(KERN_INFO"message: writing to %d \"%s\"\n", msg->minor, msg->buffer);
+    msg->len = to_copy;
+    msg->buffer[len+1] = '\0';
 
     msg->valid = 1;
-    return len;
+    return to_copy;
 }
 
